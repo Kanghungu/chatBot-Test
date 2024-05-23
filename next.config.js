7efb0b294198f228f2cc5577e801511b9d985d6c
gjsrn5698 <gjsrn5698@naver.com>
2024-05-23 16:13:50 +0900

    버전맞추기 1.1

--- next.config.js
+++ next.config.js
@@ -9,5 +9,9 @@ module.exports = {
                 pathname: '**'
             }
         ]
+    },
+    env: {
+        AUTH_SECRET: process.env.AUTH_SECRET,
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY
     }
 }
